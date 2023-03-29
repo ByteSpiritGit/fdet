36d08216e97392c2d7b06bcc8768d3604a0af43f
OriginalStefikO <69142073+OriginalStefikO@users.noreply.github.com>
2023-03-29 21:17:01 +0200

    Package.json got ignored and forgotten in darkness of my laptop-Now repaired I hope

--- svelteSource/src/App.svelte
+++ svelteSource/src/App.svelte
@@ -1,43 +1,22 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from './assets/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  // import Counter from './lib/Counter.svelte'
+  import Navbar from './lib/MainNavbar.svelte'
 </script>
 
+<!-- * HTML -->
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+  <header>
+    <logo />
+    <Navbar />
+    <profile />
+  </header>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <section>
+    
+  </section>
 </main>
 
+<!-- * CSS -->
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+  
 </style>
