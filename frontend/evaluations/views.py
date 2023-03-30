e0a32607c5d883c31fe6c7f8375d1157d3a66adf
TheLastMercy <DavidVrt2@gmail.com>
2023-03-30 22:00:08 +0200

    Idk, if this is working, but just pushing to public

--- frontend/evaluations/views.py
+++ frontend/evaluations/views.py
@@ -6,18 +6,56 @@ import requests
 # Create your views here.
 def evaluation_view(request, *args, **kwargs):
     text = request.GET["text"]
+
+
+    # text_list = text.split(". ")
+    # i = 0
+    # dicts_list = []
+    # index_dict = {}
+    # for sentence in text_list:
+    #     database_dict = {
+    #         "claim" : none,
+    #         "label" : none,
+    #         "supports" : none,
+    #         "refutes" : none,
+    #         "evidence" : none,
+    #         "id" : none
+    #     }
+    #     index_dict[i] = sentence
+    #     evaluated_sentence = Evaluation.objects.get(claim=sentence)
+    #     if evaluated_sentence:
+    #         database_dict["claim"] = evaluated_sentence.claim
+    #         database_dict["label"] = evaluated_sentence.label
+    #         database_dict["supports"] = evaluated_sentence.supports
+    #         database_dict["refutes"] = evaluated_sentence.refutes
+    #         database_dict["evidence"] = evaluated_sentence.evidence
+    #         database_dict["id"] = evaluated_sentence.id
+    #         dicts_list.append(database_dict)
+    #     i += 1
+    #     text_list.remove(sentence)
+
+    # text_back = ". ".join()
+
     validated_text = requests.get("http://127.0.0.1:8002/backend/eval", params={"text" : text}).json()
     # print(type(validated_text[0]["label"]))
 
     for evaluation in validated_text:
-        new_evaluation = Evaluation.objects.create(
-            claim=evaluation["claim"], 
-            label=evaluation["label"], 
-            supports=evaluation["supports"], 
-            refutes=evaluation["refutes"], 
-            evidence=evaluation["evidence"]
-        )
-        evaluation["id"] = new_evaluation.id # ! Adding id to the obtained JSON -> passing to feedbacks app 
+        if Evaluation.objects.filter(claim=evaluation["claim"]).exists():
+            Evaluation.objects.filter(claim=evaluation["claim"]).update(
+                label=evaluation["label"], 
+                supports=evaluation["supports"], 
+                refutes=evaluation["refutes"], 
+                evidence=evaluation["evidence"]
+            )
+        else:
+            new_evaluation = Evaluation.objects.create(
+                claim=evaluation["claim"], 
+                label=evaluation["label"], 
+                supports=evaluation["supports"], 
+                refutes=evaluation["refutes"], 
+                evidence=evaluation["evidence"]
+            )
+            evaluation["id"] = new_evaluation.id # ! Adding id to the obtained JSON -> passing to feedbacks app 
 
     context = {
         "validated" : validated_text
