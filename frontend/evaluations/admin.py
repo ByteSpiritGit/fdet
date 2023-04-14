c27a847a9879137e3db1e82304655adb6d567d3e
TheLastMercy <DavidVrt2@gmail.com>
2023-04-14 17:39:53 +0200

    Adding Models.

--- frontend/evaluations/admin.py
+++ frontend/evaluations/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Evaluation
+from .models import Evaluation_block, Evaluation
 
 # Register your models here.
 
+admin.site.register(Evaluation_block)
 admin.site.register(Evaluation)
--- frontend/evaluations/views.py
+++ frontend/evaluations/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.middleware.csrf import get_token
 from django.shortcuts import render
-from .models import Evaluation
+from .models import Evaluation_block, Evaluation
 import requests
 from django.views.decorators.csrf import csrf_protect
 
@@ -11,37 +11,17 @@ from django.views.decorators.csrf import csrf_protect
 def evaluation_view(request, *args, **kwargs):
     text = request.GET["text"]
 
-
-    # text_list = text.split(". ")
-    # i = 0
-    # dicts_list = []
-    # index_dict = {}
-    # for sentence in text_list:
-    #     database_dict = {
-    #         "claim" : none,
-    #         "label" : none,
-    #         "supports" : none,
-    #         "refutes" : none,
-    #         "evidence" : none,
-    #         "id" : none
-    #     }
-    #     index_dict[i] = sentence
-    #     evaluated_sentence = Evaluation.objects.get(claim=sentence)
-    #     if evaluated_sentence:
-    #         database_dict["claim"] = evaluated_sentence.claim
-    #         database_dict["label"] = evaluated_sentence.label
-    #         database_dict["supports"] = evaluated_sentence.supports
-    #         database_dict["refutes"] = evaluated_sentence.refutes
-    #         database_dict["evidence"] = evaluated_sentence.evidence
-    #         database_dict["id"] = evaluated_sentence.id
-    #         dicts_list.append(database_dict)
-    #     i += 1
-    #     text_list.remove(sentence)
-
-    # text_back = ". ".join()
-
     validated_text = requests.get("http://127.0.0.1:8002/backend/eval", params={"text" : text}).json()
-    # print(type(validated_text[0]["label"]))
+    # validated_text = [{"claim": "Dummy claim", "label" : "REFUTES", "supports" : 0.1457, "refutes" : 0.8543, "evidence" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quibusdam architecto velit ut distinctio culpa possimus, debitis corporis, at officiis voluptas ea modi magni omnis saepe earum! Ullam, velit recusandae. Ipsa quibusdam delectus, debitis quam quisquam quasi consectetur ab obcaecati incidunt amet labore, earum velit modi fuga ducimus dignissimos perspiciatis!"}]
+    
+    
+    # //print(type(validated_text[0]["label"]))
+
+    whole_claim = ""
+    for evaluation in validated_text:
+        whole_claim += evaluation["claim"] + ". "
+
+    new_evaluation_block = Evaluation_block.objects.create(claims=whole_claim)
 
     for evaluation in validated_text:
         if Evaluation.objects.filter(claim=evaluation["claim"]).exists():
@@ -53,17 +33,22 @@ def evaluation_view(request, *args, **kwargs):
             )
         else:
             new_evaluation = Evaluation.objects.create(
+                evaluation_block=new_evaluation_block.id,
+
                 claim=evaluation["claim"], 
                 label=evaluation["label"], 
                 supports=evaluation["supports"], 
-                refutes=evaluation["refutes"], 
+                refutes=evaluation["supports"], 
                 evidence=evaluation["evidence"]
             )
-            evaluation["id"] = new_evaluation.id # ! Adding id to the obtained JSON -> passing to feedbacks app 
+            evaluation["id"] = new_evaluation.id # ! Adding id to the obtained JSON -> passing to feedbacks app
+            # evaluation["evaluation_block"] = new_evaluation_block.id
 
     context = {
         "validated" : validated_text
     }
+
+    print(JsonResponse(context))
     return JsonResponse(context)
 
 
