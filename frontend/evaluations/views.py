49c87db6f78f41949bae117eb42f19eef8f18aee
TheLastMercy <DavidVrt2@gmail.com>
2023-04-14 18:17:01 +0200

    Adding Models

--- frontend/evaluations/views.py
+++ frontend/evaluations/views.py
@@ -12,43 +12,39 @@ def evaluation_view(request, *args, **kwargs):
     text = request.GET["text"]
 
     validated_text = requests.get("http://127.0.0.1:8002/backend/eval", params={"text" : text}).json()
-    # validated_text = [{"claim": "Dummy claim", "label" : "REFUTES", "supports" : 0.1457, "refutes" : 0.8543, "evidence" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quibusdam architecto velit ut distinctio culpa possimus, debitis corporis, at officiis voluptas ea modi magni omnis saepe earum! Ullam, velit recusandae. Ipsa quibusdam delectus, debitis quam quisquam quasi consectetur ab obcaecati incidunt amet labore, earum velit modi fuga ducimus dignissimos perspiciatis!"}]
-    
     
     # //print(type(validated_text[0]["label"]))
 
-    whole_claim = ""
-    for evaluation in validated_text:
-        whole_claim += evaluation["claim"] + ". "
-
+    # * New Evaluation Block creation
+    whole_claim = " ".join([claim["claim"] for claim in validated_text])
     new_evaluation_block = Evaluation_block.objects.create(claims=whole_claim)
 
     for evaluation in validated_text:
         if Evaluation.objects.filter(claim=evaluation["claim"]).exists():
             Evaluation.objects.filter(claim=evaluation["claim"]).update(
-                label=evaluation["label"], 
-                supports=evaluation["supports"], 
-                refutes=evaluation["refutes"], 
-                evidence=evaluation["evidence"]
+                label=evaluation.get("label"), 
+                supports=evaluation.get("suppoers"), 
+                refutes=evaluation.get("refutes"),
+                evidence=evaluation.get("evidence")
             )
         else:
             new_evaluation = Evaluation.objects.create(
-                evaluation_block=new_evaluation_block.id,
+                evaluation_block=new_evaluation_block,
 
-                claim=evaluation["claim"], 
-                label=evaluation["label"], 
-                supports=evaluation["supports"], 
-                refutes=evaluation["supports"], 
-                evidence=evaluation["evidence"]
+                claim=evaluation.get("claim"),
+                label=evaluation.get("label"), 
+                supports=evaluation.get("suppoers"), 
+                refutes=evaluation.get("refutes"),
+                ei=evaluation.get("ei"),
+                nei=evaluation.get("nei"),
+                evidence=evaluation.get("evidence")
             )
             evaluation["id"] = new_evaluation.id # ! Adding id to the obtained JSON -> passing to feedbacks app
-            # evaluation["evaluation_block"] = new_evaluation_block.id
+            evaluation["evaluation_block"] = new_evaluation_block.id
 
     context = {
         "validated" : validated_text
     }
-
-    print(JsonResponse(context))
     return JsonResponse(context)
 
 
