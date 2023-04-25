6fdc4993d2e87ea6804ddb64d26ebe44ba86e0bc
OriginalStefikO <69142073+OriginalStefikO@users.noreply.github.com>
2023-04-25 23:55:27 +0200

    Navbar and inputfield responsive

--- svelteSource/src/lib/Navbar.svelte
+++ svelteSource/src/lib/Navbar.svelte
@@ -1,7 +1,12 @@
+<script lang="ts">
+   import Login from "../mains/Login.svelte";
+
+   function showProfile() {
+      
+   }
+</script>
+
 <nav class="navbar">
-   <section class="logo-section">
-      <a href="/#/evaluations">**Logo**</a>
-   </section>
    <section class="menu-section">
       <a href="/">Home</a>
       <a href="/evaluation?text=The sky is blue">About us</a>
@@ -9,11 +14,147 @@
       <a href="https://github.com/fdet/fDet">Github</a>
       <a href="/">History</a>
    </section>
-   <section class="profile-section">
-      <a href="/#/login">**profile**</a>
+   <section class="sides-section">
+      <section class="logo-section">
+         <a href="/#/evaluations">
+            <!-- <img src="/assets/logo.png" alt="logo" width="50px" height="50px"> -->
+            **logo**
+         </a>
+      </section>
+      <section class="profile-section">
+         <a href="/#/login" on:click={showProfile}>**profile**</a>
+      </section>
    </section>
 </nav>
 
+
+<style>
+   .navbar {
+      position: absolute;
+      top: 0;
+
+      width: 100%;
+
+      font-size: 1.2em;
+   }
+   
+   .sides-section {
+      position: fixed;
+      z-index: 250;
+      top: 0;
+
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-between;
+
+      width: 100%;
+   }
+
+   .sides-section > section {
+      width: 150px;
+      background-color: var(--color-secondary);
+
+   }
+
+   .sides-section > section:first-child {
+      border-radius: 0 0 10px 0;
+      box-shadow: 0 0 10px 1px var(--color-primary);
+   }
+
+   .sides-section > section:last-child {
+      border-radius: 0 0 0 10px;
+      box-shadow: 0 0 10px 1px var(--color-primary);
+   }
+
+   .sides-section > section > a {
+      color: var(--color-text);
+      text-decoration: none;
+
+      width: 100%;
+      height: 100%;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      transition: 0.1s;
+
+      white-space: nowrap;
+
+      padding: 0 0.5em;
+      height: 2.6em;
+   }
+
+   .menu-section {
+      position: fixed;
+      z-index: 500;
+
+      width: 100%;
+
+      border-radius: 0 0 10px 10px;
+      width: fit-content;
+
+      margin: 0 auto;
+      left: 0;
+      right: 0;
+
+
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-evenly;
+
+      overflow: hidden;
+
+      background-color: var(--color-secondary);
+   }
+
+   .menu-section > a {
+      color: var(--color-text);
+      text-decoration: none;
+
+      width: 100%;
+      height: 100%;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      transition: 0.1s;
+
+      white-space: nowrap;
+
+      padding: 0 0.5em;
+      height: 2.6em;
+   }
+
+   .menu-section > a:hover {
+      background-color: var(--color-tertiary);
+   }
+
+   @media (max-width: 680px) {
+      .sides-section {
+         position: fixed;
+         top: 2.6em;
+      }
+      .sides-section > section:first-child {
+         border-radius: 0 0 10px 0;
+      }
+
+      .sides-section > section:last-child {
+         border-radius: 0 0 0 10px;
+      }
+      
+      .menu-section {
+         border-radius: 0 0 0 0;
+
+         width: 100%;
+      }
+   }
+</style>
+
+<!-- 
 <style>
    @import "../main.css";
 
@@ -80,4 +221,4 @@
    .navbar > .menu-section > a:hover {
       background-color: var(--color-tertiary);
    }
-</style>
+</style> -->
--- svelteSource/src/lib/WhatWeDo.svelte
+++ svelteSource/src/lib/WhatWeDo.svelte
@@ -15,17 +15,122 @@
    }
 </script>
 
-<section>
-   <div class="mask" bind:this={img}>
-      <img src="{whatWeDoPng}" alt="What we do">
-   </div>
-</section>
+
+<section class="whatWeDo">
+   <section class="block">
+      <h3>You state a fact</h3>
+      <p>"The human body is made up of about 60% water"</p>
+   </section>
+   <div class="hyphen"></div>
+   <section class="block">
+      <h3>We find the evidence</h3>
+      <p>"The human body contains from 55% to 78% of water"</p>
+   </section>
+   <div class="hyphen"></div>
+   <section class="block">
+      <h3>And return results</h3>
+      <p>"Supports: 83.74% <br /> Refutes: 16.26%"</p>
+   </section>
+</section>   
 
 <style>
+   .whatWeDo {
+      position: relative;
+
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-between;
+
+      max-width: 45em;
+
+      height: 100%;
+
+      margin: 0 auto;
+      margin-top: 70px;
+   }
+
+   .hyphen {
+      width: 2em;
+      height: 0.25em;
+
+      border-radius: 2em;
+
+      background-color: var(--color-text);
+
+      margin: 0 1em;
+   }
+
+   h3 {
+      margin: 0;
+      /* margin-bottom: 0.2em; */
+   }
+
+   p {
+      margin: 0;
+      margin-top: 0.2em;
+
+      /* text-align: center; */
+      color: var(--color-text-darker);
+   }
+
+   .block {
+      width: 200px;
+   }
+
+   
+   @media (max-width: 768px) {
+      .whatWeDo {
+         flex-direction: column;
+
+         margin: 30px;
+
+         /* margin-top: 125px; */
+      }
+
+      .hyphen {
+         width: 0.25em;
+         height: 1em;
+
+         margin: 0.5em 0;
+      }
+
+      p {
+         text-align: center;
+      }
+
+      .block {
+         display: flex;
+         flex-direction: column;
+         align-items: center;
+         justify-content: center;
+      }
+   }
+
+   @media (max-height: 770px) and (max-width: 768px) {
+      .whatWeDo {
+         margin-top: 125px;
+      }
+   }
+
+   @media (max-height: 640px) and (min-width: 768px) {
+      .whatWeDo {
+         margin-top: 125px;
+      }
+   }
+</style>
+
+<!-- <section>
+   <div class="mask" bind:this={img}>
+      <img src={whatWeDoPng} alt="What we do" />
+   </div>
+</section> -->
+
+<!-- <style>
    .mask {
       background-color: var(--color-text);
-      width: 45em;
-      /* min-width: 700px; */
+      max-width: 45em;
+      width: 90%;
       margin: 0px auto;
 
       margin-top: 70px;
@@ -34,8 +139,8 @@
    .mask img {
       /* display: none; */
       opacity: 0;
-      
+
       width: 100%;
       height: 100%;
    }
-</style>
+</style> -->
