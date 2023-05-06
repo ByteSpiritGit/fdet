9853a0c017d3d38a0f593e1a20fcb33a1eb6f1a9
TheLastMercy <DavidVrt2@gmail.com>
2023-05-06 13:58:25 +0200

    Fixed the issues

--- frontend/evaluations/views.py
+++ frontend/evaluations/views.py
@@ -3,37 +3,36 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 from django.middleware.csrf import get_token
 from django.shortcuts import render, redirect
 from .models import Evaluation_block, Evaluation
-import re
+import re, unicodedata
 import requests
 import queue
 
-input_text_regex = re.compile(r'^[\\x01-\\x7E\\p{L}\\p{N}\\s]+$')
+# input_text_regex = re.compile(r'^[\x20-\x7E\p{L}\p{N}]+$')
 request_queue = queue.Queue()
 
-def create_evaluation_fnc(evaluation_block, evaluation):
+def create_evaluation_fnc(evaluation_block, evaluation_dict):
     new_evaluation = Evaluation.objects.create(
         evaluation_block=evaluation_block,
 
-        claim=evaluation.get("claim"),
-        label=evaluation.get("label"), 
-        supports=evaluation.get("supports"), 
-        refutes=evaluation.get("refutes"),
-        ei=evaluation.get("ei"),
-        nei=evaluation.get("nei"),
-        evidence=evaluation.get("evidence"),
-        justify=evaluation.get("justify"),
-        url=evaluation.get("url")
+        claim=evaluation_dict.get("claim"),
+        label=evaluation_dict.get("label"), 
+        supports=evaluation_dict.get("supports"), 
+        refutes=evaluation_dict.get("refutes"),
+        ei=evaluation_dict.get("ei"),
+        nei=evaluation_dict.get("nei"),
+        evidence=evaluation_dict.get("evidence"),
+        justify=evaluation_dict.get("justify"),
+        url=evaluation_dict.get("url")
     )
-    evaluation["id"] = new_evaluation.id # ! Adding id to the obtained JSON -> passing to feedbacks app
-    evaluation["evaluation_block"] = evaluation_block.id
+    evaluation_dict["id"] = new_evaluation.id # ! Adding id to the obtained JSON -> passing to feedbacks app
+    evaluation_dict["evaluation_block"] = evaluation_block.id
 
 
 def eval_fnc(request, input_text, backend_url):
 
     # # * Check for invalid characters
-    # if not re.match(input_text_regex, input_text):
-    #     # return JsonResponse({"error": "Invalid input. The input contains invalid characters."}, status=400)
-    #     return HttpResponse(status=400)
+    # if not input_text_regex.match(input_text):
+    #     return JsonResponse({"error_msg": "Invalid input. The input contains invalid characters.", "status": 400})    
 
     # * Check for authentication
     user = request.user
@@ -49,14 +48,14 @@ def eval_fnc(request, input_text, backend_url):
             queued_request, queued_input_text, queued_backend_url = request_queue.get()
             queued_validated_text = requests.get(queued_backend_url, params={"text": queued_input_text})
             
-            if queued_validated_text.get("status_code") == 503:
+            if queued_validated_text.status_code == 503:
                 # Pokud je stále kód 503, přidejte request zpět na konec fronty
                 request_queue.put((queued_request, queued_input_text, queued_backend_url))
                 break
             else:
                 # Pokud kód 503 není, zpracujte request
                 queued_request_json = eval_fnc(queued_request, queued_input_text, queued_backend_url)
-                if queued_request_json.get("status") == 503:
+                if queued_request_json.status_code == 503:
                     # Pokud byl request opět přidán do fronty, přidejte ho zpět na konec fronty
                     request_queue.put((queued_request, queued_input_text, queued_backend_url))
                     break
@@ -69,7 +68,7 @@ def eval_fnc(request, input_text, backend_url):
         for evaluation in validated_text.json():
             create_evaluation_fnc(new_evaluation_block, evaluation)
 
-        return JsonResponse({"validated" : validated_text})
+        return JsonResponse({"validated" : validated_text.json()})
         
     else:
         return HttpResponse(status=401)
@@ -84,11 +83,11 @@ def v1_dummy_fnc_backend_view(request):
 
 def v1_evaluation_view(request, *args, **kwargs):
     text = request.GET["text"]
-    eval_fnc(request, text, "http://127.0.0.1:8002/backend/v1/eval")
+    return eval_fnc(request, text, "http://127.0.0.1:8002/backend/v1/eval")
 
 def v1_evaluation_fast_view(request, *args, **kwargs):
     text = request.GET["text"]
-    eval_fnc(request, text, "http://127.0.0.1:8002/backend/v1/eval_fast")
+    return eval_fnc(request, text, "http://127.0.0.1:8002/backend/v1/eval_fast")
 
 
 # ! RAG
@@ -100,15 +99,15 @@ def rag_dummy_fnc_backend_view(request):
 
 def rag_evaluation_view(request, *args, **kwargs):
     text = request.GET["text"]
-    eval_fnc(request, text, "http://127.0.0.1:8002/backend/rag/eval")
+    return eval_fnc(request, text, "http://127.0.0.1:8002/backend/rag/eval")
 
 def rag_evaluation_DPR_view(request, *args, **kwargs):
     text = request.GET["text"]
-    eval_fnc(request, text, "http://127.0.0.1:8002/backend/rag/eval_DPR")
+    return eval_fnc(request, text, "http://127.0.0.1:8002/backend/rag/eval_DPR")
     
 def rag_evaluation_Ada_view(request, *args, **kwargs):
     text = request.GET["text"]
-    eval_fnc(request, text, "http://127.0.0.1:8002/backend/rag/eval_ada")
+    return eval_fnc(request, text, "http://127.0.0.1:8002/backend/rag/eval_ada")
 
 def rag_evaluation_BM25_view(request, *args, **kwargs):
     text = request.GET["text"]
