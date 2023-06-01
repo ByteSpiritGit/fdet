8c95318f7e7a8cac09c9a02e13e4e97700526a31
Dzeniks <59582869+Dzeniks@users.noreply.github.com>
2023-06-01 22:17:12 +0200

    Create dockerfiles for backend and frontend, change views backend host and port, update gitignore

---
+++ server_run.bat
@@ -0,0 +1,18 @@
+@echo
+pip install -r requirements.txt --index-url https://download.pytorch.org/whl/cu118
+
+REM Installation of NPM packages
+cd /d "svelteSource"
+start cmd.exe /c "npm install", "npm run build"
+cd /d ".."
+
+cd /d "frontend"
+start cmd.exe /k "python manage.py runserver"
+cd /d ".."
+
+REM FAST API start command
+cd /d "backend"
+::uvicorn main:app --host 127.0.0.1 --port 8002 
+start cmd.exe /k "python api.py"
+cd /d ".."
+cmd /k
