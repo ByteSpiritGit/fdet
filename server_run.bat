10bddd4ef9c6a3cf55f839cabb18be327dbed485
Jan Soukeník <59582869+Dzeniks@users.noreply.github.com>
2023-08-31 00:28:07 +0200

    Readme update screenshots (#31)
    * Update readme and add screenshots
    
    * requirements.txt update, change in server_run.bat

--- server_run.bat
+++ server_run.bat
@@ -1,12 +1,12 @@
 @echo
 pip install -r requirements.txt --index-url https://download.pytorch.org/whl/cu118
-
 REM Installation of NPM packages
 cd /d "svelteSource"
 start cmd.exe /c "npm install", "npm run build"
 cd /d ".."
 
 cd /d "frontend"
+python manage.py migrate
 start cmd.exe /k "python manage.py runserver"
 cd /d ".."
 
